2f10e6e6ae3bad7d9ce2783ea85e52e9be7bbb1c
noxtla <noxtlaxs@gmail.com>
2025-03-30 12:07:10 -0600

    Nivel 0 completado: Estructura inicial del proyecto de spring Boot

---
+++ springboot-jwt/src/main/java/com/noxtla/gamificacion/springboot/app/springboot_jwt/SpringbootJwtApplication.java
@@ -0,0 +1,13 @@
+package com.noxtla.gamificacion.springboot.app.springboot_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJwtApplication.class, args);
+	}
+
+}
